90858c14456c08106808fa0e49935eb229dc9e5e
francesco.miscia <fra.miscia@gmail.com>
2023-02-28 14:51:10 +0100

    Conditional rendering on JSX

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,19 @@
 import './App.css'
 
 function App() {
-  const name = "Pippo"
-  const otherName = "Pluto"
   const toggle = false
   return (
-    <div className="App">
-      <div className="card">
-        <button onClick={() => {}}>
-          Click Me {toggle ? name : otherName}
-        </button>
+      <div className="App">
+          {toggle ? (
+              <div className="card">
+                  <button onClick={() => {}}>Button 0</button>
+              </div>
+          ) : (
+              <div className="card">
+                  <button onClick={() => {}}>Button 0</button>
+              </div>
+          )}
       </div>
-    </div>
   )
 }
 
