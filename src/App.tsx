f1162b34f4d8c103e37647d74922fd5f90e9e9e5
francesco.miscia <fra.miscia@gmail.com>
2023-02-28 15:12:22 +0100

    Render complex lists

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,20 @@
 import './App.css'
+import { contacts } from '../db'
 
 function App() {
-    const contacts = ["Alessandro", "Luca", "Paolo", "Mario"]
     return (
         <div className="App">
-            {contacts.map((it) => (
-                <div className="card">
-                    <h3>{it}</h3>
-                    <button onClick={() => {}}>Click Me</button>
-                </div>
-            ))}
+            <div style={{ display: 'flex', flexDirection: 'row', flexWrap: 'wrap' }}>
+                {contacts.map((it) => (
+                    <div className="card">
+                        <h3>{it.name}</h3>
+                        <p>{it.email}</p>
+                        <p>{it.phone}</p>
+                        <p>{it.notes}</p>
+                        <button onClick={() => {}}>Click Me</button>
+                    </div>
+                ))}
+            </div>
         </div>
     )
 }
