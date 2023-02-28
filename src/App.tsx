9a1da538fa660a653769dc34e3322f4c85819ee2
francesco.miscia <fra.miscia@gmail.com>
2023-02-28 14:50:04 +0100

    Simple conditional rendering

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,14 @@
 import './App.css'
 
 function App() {
+  const name = "Pippo"
+  const otherName = "Pluto"
+  const toggle = false
   return (
     <div className="App">
       <div className="card">
         <button onClick={() => {}}>
-          Click Me
+          Click Me {toggle ? name : otherName}
         </button>
       </div>
     </div>
