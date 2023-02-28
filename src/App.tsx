74b93f3d8534a14b8cbd9563e8b6496d77d802a0
francesco.miscia <fra.miscia@gmail.com>
2023-02-28 15:38:43 +0100

    useState and render cycles

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,19 @@
 import './App.css'
 import { contacts } from '../db'
 import ContactCard from './components/ContactCard'
+import { useState } from 'react'
+import Button from './components/Button'
 
 function App() {
+    const [number, setNumber] = useState(1)
     return (
         <div className="App">
+            <h2>COUNTER: {number}</h2>
+            <Button label='INCREMENT' onClick={() => {
+                setNumber(number + 1)
+                setNumber(number + 1)
+                setNumber(number + 5)
+            }} />
             <div style={{ display: 'flex', flexDirection: 'row', flexWrap: 'wrap' }}>
                 {contacts.map((it) => (
                     <ContactCard key={it.id} {...it} />
